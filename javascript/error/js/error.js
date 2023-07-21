8c761543aa3b366ed197ff5857305ddbd060b002
phym <135787491+Phym-SaE@users.noreply.github.com>
2023-07-21 18:37:54 +0900

    Delete javascript directory

--- javascript/error/js/error.js
+++
@@ -1,9 +0,0 @@
-const resetBtn = document.getElementsById('reset-btn');
-
-/* ----------------------------------------------------
-    リセットボタン
----------------------------------------------------- */
-resetBtn.addEventListener('click', () => {
-    let resultArea = document.querySelector('#comment');
-    resultArea.remove();
-})
