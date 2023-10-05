4eaa0ebe22810568f951efa48441772530edf4e5
phym <135787491+Phym-SaE@users.noreply.github.com>
2023-10-05 12:09:28 +0900

    upload kadai_sort

---
+++ php-basic-kadai/kadai_sort/sort.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10];
+
+        function sort_2way($array, $order)
+        {
+            if ($order) {
+                echo '昇順にソートします。<br />';
+                sort($array);
+            } else {
+                echo '降順にソートします。<br />';
+                rsort($array);
+            }
+            foreach ($array as $num) {
+                echo $num . '<br />';
+            }
+            echo '<br />';
+        }
+        $array = $nums;
+        $order = true;
+        sort_2way($array, $order);
+        $order = false;
+        sort_2way($array, $order);
+        ?>
+    </p>
+</body>
+
+</html>
